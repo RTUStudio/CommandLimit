008cc3a5b83034351a147f92c5199be17e35f205
nyaon08 <jnk1io@naver.com>
2025-06-18 17:58:34 +0900

    CommandLimit 1.2.9 RSF : Update to 2.5.0

--- src/main/java/com/github/ipecter/rtustudio/commandlimit/command/MainCommand.java
+++ src/main/java/com/github/ipecter/rtustudio/commandlimit/command/MainCommand.java
@@ -4,13 +4,14 @@ import com.github.ipecter.rtustudio.commandlimit.CommandLimit;
 import com.github.ipecter.rtustudio.commandlimit.configuration.LimitConfig;
 import kr.rtuserver.framework.bukkit.api.command.RSCommand;
 import kr.rtuserver.framework.bukkit.api.command.RSCommandData;
+import org.bukkit.permissions.PermissionDefault;
 
 public class MainCommand extends RSCommand<CommandLimit> {
 
     private final LimitConfig limitConfig;
 
     public MainCommand(CommandLimit plugin) {
-        super(plugin, "cmdlimit");
+        super(plugin, "cmdlimit", PermissionDefault.OP);
         this.limitConfig = plugin.getLimitConfig();
     }
 
